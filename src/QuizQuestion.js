26e1f674c322cc24586c54a759ff9ae6f243f8f8
ai_ern <im_in@outlook.sg>
2020-07-06 22:09:31 +0800

    Completed module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,6 +3,11 @@ import QuizQuestionButton from './QuizQuestionButton'
 
 class QuizQuestion extends Component {
 
+handleClick (buttonText) {
+    if(buttonText === this.props.quiz_question.answer) {
+        this.props.showNextQuestionHandler()
+        }
+    }
 render () {
     return (
         <main>
@@ -13,7 +18,10 @@ render () {
     
         <section className="buttons">
           <ul>
-              <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+              {this.props.quiz_question.answer_options.map((answer_option, index) => {
+                  return <QuizQuestionButton key={index} button_text={answer_option}
+                  clickHandler={this.handleClick.bind(this)} />
+              })}
             
           </ul>
         </section>
